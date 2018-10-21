06c0ce53679c1f7a9987846b5b08270c0c41beda
Vipin Maurya <37982085+ivipinmaurya@users.noreply.github.com>
2018-10-21 15:55:19 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -161,6 +161,8 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + " restaurant's image";
+  image.tabIndex = 0;
   li.append(image);
 
   const name = document.createElement('h1');
